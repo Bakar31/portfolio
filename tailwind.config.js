f68706a9689084e40b0e452d6f595ab59fde1308
Jalal Uddin <dgvai.hridoy@gmail.com>
2023-06-24 14:43:16 +0600

    chore: init tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      tinker: "#54B689",
+      linter: "#306D53",
+      whiteboard: "#ffffff",
+      pencil: "#434343",
+      pen: "#292929",
+      board: "#1E2A3A",
+      midnight: "#111821",
+      charcoal: "#384F6C",
     },
   },
   plugins: [],
-}
+};
