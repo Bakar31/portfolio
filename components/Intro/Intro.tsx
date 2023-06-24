f5e2f499b7eee071e383df001deb9676a35084f0
Jalal Uddin <dgvai.hridoy@gmail.com>
2023-06-24 22:19:25 +0600

    feat: add Intro

---
+++ components/Intro/Intro.tsx
@@ -0,0 +1,29 @@
+import { Button } from "flowbite-react";
+import { HiDocumentText } from "react-icons/hi";
+import { FaTelegramPlane } from "react-icons/fa";
+
+export const Intro = () => (
+  <div className="flex flex-col gap-1">
+    <h1 className="text-5xl font-bold">Jalal Uddin</h1>
+    <span className="text-2xl font-light">Software Engineer &bull; Machine Learning Enthusiast</span>
+    <p className="text-justify font-light w-2/3 text-sm">
+      An enthusiastic and dynamic programmer with a relentless curiosity for problem-solving, possessing a versatile
+      proficiency in coding across various languages and platforms. Extensively experienced in developing web
+      applications in conjunction with a knack for crafting software architecture design, modeling, and maintaining
+      scalable infrastructure for efficient development operations. Furthermore, my research in machine learning and
+      data science has fostered a deep understanding of data-efficient software engineering. Seeking opportunities to
+      further expand my research knowledge and contribute to groundbreaking advancements. Committed to staying abreast
+      of the latest industry trends and continuously honing my skills through self-directed learning and participation.
+    </p>
+    <div className="flex gap-4 my-4">
+      <Button className="bg-tinker hover:bg-linter">
+        <HiDocumentText className="mr-2" />
+        <p>Email Me</p>
+      </Button>
+      <Button className="bg-pen hover:bg-midnight">
+        <FaTelegramPlane className="mr-2" />
+        <p>Leave a note</p>
+      </Button>
+    </div>
+  </div>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
+import { Intro } from "@/components/Intro";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
-import { Tooltip } from "flowbite-react";
 
 export default function Home() {
   return (
     <main className="w-full text-whiteboard">
-      <Sidebar />
-      <div className="ml-72 min-h-screen bg-whiteboard text-midnight flex flex-col p-2">
-        <div className="flex h-screen">larged</div>
-        <div className="flex h-screen">larged</div>
-        <div className="flex h-screen">larged</div>
-        <div className="flex h-screen">larged</div>
+      <Sidebar className="w-72 min-h-screen bg-tinker flex flex-col p-2 fixed" />
+      <div className="ml-72 min-h-screen bg-whiteboard text-midnight flex flex-col p-10">
+        <Intro />
       </div>
     </main>
   );
