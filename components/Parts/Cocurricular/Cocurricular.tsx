f634868dd69936c6855ae9e7ee20307426e22bdf
Jalal Uddin <dgvai.hridoy@gmail.com>
2023-07-12 22:59:43 +0600

    refactor: redsign cocurricular section

--- components/Parts/Cocurricular/Cocurricular.tsx
+++ components/Parts/Cocurricular/Cocurricular.tsx
@@ -8,38 +8,42 @@ import { Section } from "@/components/Section";
 export const Cocurricular = () => (
   <Section title="Extracurricular Activities" Icon={GiBiceps}>
     {activities.map((activity, i) => (
-      <div key={i} className="iterator-section">
-        <span className="text-tinker font-bold text-xl">{activity.designation}</span>
-        <span className="font-light text-pencil">{activity.organization}</span>
-        <div className="flex gap-4 text-xs font-light mt-2">
-          <div className="flex gap-2 items-center">
-            <FaMapMarkerAlt className="text-tinker" />
-            <span>{activity.location}</span>
-          </div>
-          <div className="flex gap-2 items-center">
-            <FaClock className="text-tinker" />
-            <span>{activity.duration}</span>
+      <div key={i} className="hoverable-card group">
+        <div className="flex flex-col w-3/6">
+          <span className="hoverable-card-title">{activity.designation}</span>
+          <span className="hoverable-card-subtitle">{activity.organization}</span>
+          <div className="flex gap-1 mt-2">
+            {activity.skills.map((item, k) => (
+              <Badge color={"success"} key={k}>
+                {item}
+              </Badge>
+            ))}
           </div>
         </div>
-        <div className="flex gap-1 mt-2">
-          {activity.skills.map((item, k) => (
-            <Badge color={"success"} key={k}>
-              {item}
-            </Badge>
+        <div className="flex flex-col w-2/6 gap-2">
+          {activity.formers?.map((exActivity, x) => (
+            <div className="flex flex-col" key={x}>
+              <div className="flex items-center gap-2">
+                <BsFillBriefcaseFill className="hoverable-card-icon" />
+                <span className="hoverable-card-text">{exActivity.designation}</span>
+              </div>
+              <div className="flex items-center gap-2 text-xs font-light">
+                <FaClock className="hoverable-card-icon" />
+                <span className="hoverable-card-text">{exActivity.duration}</span>
+              </div>
+            </div>
           ))}
         </div>
-        {activity.formers?.map((exActivity, x) => (
-          <div className="flex flex-col ml-8 border-l-2 pl-2 mt-2 hover:ml-10 transition-all duration-200" key={x}>
-            <div className="flex items-center gap-2 text-tinker text-sm font-normal">
-              <BsFillBriefcaseFill />
-              <span>{exActivity.designation}</span>
-            </div>
-            <div className="flex items-center gap-2 text-xs font-light text-charcoal">
-              <FaClock />
-              <span>{exActivity.duration}</span>
-            </div>
+        <div className="flex flex-col gap-2 text-xs font-light w-1/6">
+          <div className="flex gap-2 items-center">
+            <FaMapMarkerAlt className="hoverable-card-icon" />
+            <span className="hoverable-card-text">{activity.location}</span>
           </div>
-        ))}
+          <div className="flex gap-2 items-center">
+            <FaClock className="hoverable-card-icon" />
+            <span className="hoverable-card-text">{activity.duration}</span>
+          </div>
+        </div>
       </div>
     ))}
   </Section>
