2bba912577bfc5f9aad77f0bbc684e3f64ce0695
Jalal Uddin <dgvai.hridoy@gmail.com>
2023-07-12 14:32:52 +0600

    feat: redesign experience

--- components/Parts/Experience/Experience.constants.ts
+++ components/Parts/Experience/Experience.constants.ts
@@ -6,17 +6,20 @@ export const experiences: IExperience[] = [
     positon: "Software Engineer",
     location: "Dhaka, Bangladesh",
     duration: "Sep 2022 - Present",
+    logo: "https://i.ibb.co/kMgbkbN/sazim.jpg",
   },
   {
     institute: "Charity Guard ApS",
     positon: "API Developer (Remote/Contractual)",
     location: "Odense, Denmark",
     duration: "Aug 2020 - Aug 2021",
+    logo: "https://i.ibb.co/4WDHSb2/rsz-2charity-guard-logo-2018-1.png",
   },
   {
     institute: "Step-up IT & Training Institute",
     positon: "Full-stack Developer (Part-time)",
     location: "Rajshahi, Bangladesh",
     duration: "Jul 2019 - Sep 2020",
+    logo: "https://i.ibb.co/VjVkbgg/rsz-stup.png",
   },
 ];
--- components/Parts/Experience/Experience.tsx
+++ components/Parts/Experience/Experience.tsx
@@ -2,21 +2,39 @@ import { FaClock, FaMapMarkerAlt } from "react-icons/fa";
 import { ImOffice } from "react-icons/im";
 import { experiences } from "./Experience.constants";
 import { Section } from "@/components/Section";
+import Image from "next/image";
 
 export const Experience = () => (
   <Section Icon={ImOffice} title="Professional Experiences">
     {experiences.map((experience, i) => (
-      <div key={i} className="iterator-section">
-        <span className="text-tinker font-bold text-xl">{experience.institute}</span>
-        <span className="font-light text-pencil">{experience.positon}</span>
-        <div className="flex gap-4 text-xs font-light mt-2">
+      <div key={i} className="hoverable-card group">
+        <Image
+          className="group-hover:scale-105 transition-all duration-500"
+          src={experience.logo}
+          width={50}
+          height={50}
+          alt={experience.institute}
+        />
+        <div className="flex flex-grow flex-col group-hover:ml-4 transition-all duration-500">
+          <span className="text-tinker group-hover:text-whiteboard font-bold text-xl transition-all duration-500">
+            {experience.institute}
+          </span>
+          <span className="font-bold text-sm text-charcoal group-hover:text-whiteboard transition-all duration-500">
+            {experience.positon}
+          </span>
+        </div>
+        <div className="flex flex-col gap-2 text-sm font-light mt-2 w-64">
           <div className="flex gap-2 items-center">
-            <FaMapMarkerAlt className="text-tinker" />
-            <span>{experience.location}</span>
+            <FaMapMarkerAlt className="text-tinker group-hover:text-whiteboard transition-all duration-500" />
+            <span className="text-charcoal group-hover:text-whiteboard transition-all duration-500">
+              {experience.location}
+            </span>
           </div>
           <div className="flex gap-2 items-center">
-            <FaClock className="text-tinker" />
-            <span>{experience.duration}</span>
+            <FaClock className="text-tinker group-hover:text-whiteboard transition-all duration-500" />
+            <span className="text-charcoal group-hover:text-whiteboard transition-all duration-500">
+              {experience.duration}
+            </span>
           </div>
         </div>
       </div>
