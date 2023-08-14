91c9844164465f34428c89b967a319d3296f27d4
Jalal Uddin <dgvai.hridoy@gmail.com>
2023-08-14 14:02:12 +0600

    feat: upgrade intro

--- components/Parts/Intro/Intro.tsx
+++ components/Parts/Intro/Intro.tsx
@@ -1,12 +1,13 @@
 import { Button } from "flowbite-react";
 import { HiDocumentText } from "react-icons/hi";
 import { FaTelegramPlane } from "react-icons/fa";
+import Image from "next/image";
 
 export const Intro = () => (
-  <div className="flex flex-col gap-1 text-center sm:text-left">
+  <div className="flex flex-col gap-1 text-center">
     <h1 className="text-5xl font-bold">Jalal Uddin</h1>
     <span className="text-lg sm:text-2xl font-light">Software Engineer &bull; Machine Learning Enthusiast</span>
-    <p className="text-justify font-light w-full sm:w-2/3 text-sm">
+    <p className="text-justify font-light w-full sm:w-2/3 text-sm mx-auto">
       An enthusiastic and dynamic programmer with a relentless curiosity for problem-solving, possessing a versatile
       proficiency in coding across various languages and platforms. Extensively experienced in developing web
       applications in conjunction with a knack for crafting software architecture design, modeling, and maintaining
@@ -15,7 +16,7 @@ export const Intro = () => (
       further expand my research knowledge and contribute to groundbreaking advancements. Committed to staying abreast
       of the latest industry trends and continuously honing my skills through self-directed learning and participation.
     </p>
-    <div className="flex gap-4 my-4 justify-center sm:justify-start">
+    <div className="flex gap-4 my-4 justify-center">
       <Button className="bg-tinker hover:bg-linter" href="mailto:dgvai.hridoy@gmail.com">
         <HiDocumentText className="mr-2" />
         <p>Email Me</p>
